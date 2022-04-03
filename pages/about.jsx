5641bee7ebdb67d94749d6fa500a7fbdd058d64c
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-04-03 22:13:57 +0700

    add about page

---
+++ pages/about.jsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export default function about() {
+  return (
+    <>
+      <Head>
+        <title>Kebudayaan Nusantara | About</title>
+        <meta name="description" content="About Kebudayaan Nusantara" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div className="flex flex-col items-center justify-center h-screen px-64">
+          <h1 className="text-6xl font-semibold text-center">About</h1>
+        </div>
+      </main>
+    </>
+  );
+}
