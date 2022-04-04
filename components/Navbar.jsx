34585256a01d36178e2dde2498e649def4588792
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-04-04 15:18:54 +0700

    change navbar bg-color and add container

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="fixed w-screen bg-blue-500">
-      <div className="container flex gap-5 px-64 py-2 text-2xl text-slate-100">
+    <nav className="fixed flex justify-center w-screen bg-slate-800">
+      <div className="container flex justify-end gap-8 px-64 py-2 text-2xl text-slate-100">
         <Link href="/">
           <a>Home</a>
         </Link>
